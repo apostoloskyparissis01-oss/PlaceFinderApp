a97d1b63a5450d8aac34d49e5cb4c3b8fbbb14d2
Apostolos <apostoloskyparissis01@gmail.com>
2026-03-14 20:21:31 +0200

    update backend files

--- backend/app.py
+++ backend/app.py
@@ -11,26 +11,50 @@ CORS(app)
 
 API_KEY = os.getenv('GOOGLE_API_KEY')
 
+def get_coordinates(location):
+        url = 'https://maps.googleapis.com/maps/api/geocode/json'
+        params = {
+            'address': location,
+            'key': API_KEY
+        }
+
+        response = requests.get(url, params=params)
+        data = response.json()
+
+        if data['results']:
+            lat = data['results'][0]['geometry']['location']['lat']
+            lng = data['results'][0]['geometry']['location']['lng']
+            return f'{lat},{lng}'
+        
+        return None
+
 @app.route('/')
 def home():
     return 'The server works'
 
 @app.route('/places')
 def get_places():
-    location = request.args.get('location', '37.9838,23.7275')
+    location = request.args.get('location', 'Athens')
     radius = request.args.get('radius', 1500)
     cuisine = request.args.get('cuisine', 'restaurant')
     budget = request.args.get('budget', None)
 
+    coordinates = get_coordinates(location)
+
+    if not coordinates:
+        return jsonify({'error': 'Location not found'}), 404
+
     url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
     params = {
-        'location': location,
+        'location': coordinates,
         'radius': radius,
         'type': 'restaurant',
         'keyword': cuisine,
         'key': API_KEY
 
     }
+
+    
 
     response = requests.get(url,params=params)
     data = response.json()
